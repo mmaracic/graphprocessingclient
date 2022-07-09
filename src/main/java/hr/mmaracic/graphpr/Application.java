1be6767132e506a4142fd09db12faa37aed18acb
mmaracic <marijo.maracic@gmail.com>
2022-07-09 02:08:11 +0200

    Initial commit

---
+++ src/main/java/hr/mmaracic/graphpr/Application.java
@@ -0,0 +1,14 @@
+package hr.mmaracic.graphpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
