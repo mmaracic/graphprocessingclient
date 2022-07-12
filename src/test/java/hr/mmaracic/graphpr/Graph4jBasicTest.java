06a0950f2fd7a4725180cd0229bccbc769c4376c
mmaracic <marijo.maracic@gmail.com>
2022-07-13 01:52:36 +0200

    Added apoc basic test

--- src/test/java/hr/mmaracic/graphpr/Graph4jBasicTest.java
+++ src/test/java/hr/mmaracic/graphpr/Graph4jBasicTest.java
@@ -1,7 +1,9 @@
 package hr.mmaracic.graphpr;
 
+import apoc.ApocConfig;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.neo4j.configuration.GraphDatabaseSettings;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.Session;
 import org.neo4j.harness.Neo4j;
@@ -11,19 +13,36 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
+import java.io.File;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest
 class Graph4jBasicTest {
 
-    private static final Neo4j embeddedDatabaseServer = neo4j();
+    private static final Neo4j embeddedDatabaseServer;
 
-    private static Neo4j neo4j() {
+    static {
+        try {
+            embeddedDatabaseServer = neo4j();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Neo4j neo4j() throws URISyntaxException {
+        // We need the directory containing the APOC jar, otherwise all APOC procedures must be loaded manually.
+        // While the intuitive idea might be not having APOC on the class path at all in that case and just dump
+        // it into the plugin directory, it doesn't work as APOC needs some extension factories to work with
+        // and those are not loaded from the plugin unless it's part of the original class loader that loaded neo.
+        // If you know which methods you're a gonna use, you can configure them manually instead.
+        var pluginDirContainingApocJar = new File(
+                ApocConfig.class.getProtectionDomain().getCodeSource().getLocation().toURI())
+                .getParentFile().toPath();
         return Neo4jBuilders.newInProcessBuilder()
                 .withDisabledServer() // No need for http
                 .withFixture(""
@@ -31,6 +50,8 @@ class Graph4jBasicTest {
                         + "CREATE (TheMatrixReloaded:Movie {title:'The Matrix Reloaded', released:2003, tagline:'Free your mind'})\n"
                         + "CREATE (TheMatrixRevolutions:Movie {title:'The Matrix Revolutions', released:2003, tagline:'Everything that has a beginning has an end'})\n"
                 )
+                .withConfig(GraphDatabaseSettings.plugin_dir, pluginDirContainingApocJar)
+                .withConfig(GraphDatabaseSettings.procedure_unrestricted, List.of("apoc.*"))
                 .build();
 
         // For enterprise use
@@ -63,6 +84,17 @@ class Graph4jBasicTest {
                 .map(n -> n.get("title").asString())
                 .collect(Collectors.toList());
         assertThat(titles.size(), equalTo(3));
+    }
+
+    @Test
+    void apocTest() {
+        assertThat(driver, notNullValue(Driver.class));
+        Session session = driver.session();
+        assertThat(session, notNullValue(Session.class));
+        List<String> procedures = session.run("CALL apoc.help('text')").stream()
+                .map(r -> r.get("name").asString())
+                .collect(Collectors.toList());
+        assertThat(procedures.size(), equalTo(50));
     }
 
 }
